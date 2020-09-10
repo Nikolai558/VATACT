879a1bb0d17b9ad9155295e656d13a0716639f70
Nikolas <38259407+Nikolai558@users.noreply.github.com>
2020-09-10 16:22:12 -0600

    Added Configure Artcc Form
    Added the ConfigureArtccForm, nothing is wired up on this form. Just did the design and UI elements.

---
+++ VatactUI/ConfigureArtccForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VatactUI
+{
+    public partial class ConfigureArtccForm : Form
+    {
+        public ConfigureArtccForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
